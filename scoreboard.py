8ddbc167944868a391dfe2dc2ca8d914caba68d5
moralesalex89 <moralesalex89@yahoo.com>
2019-10-07 15:11:08 -0700

    Initial Push to Github

--- scoreboard.py
+++ scoreboard.py
@@ -3,15 +3,17 @@ from pygame.sprite import Group
 
 from ship import Ship
 
+
 class Scoreboard():
 
-    def __init__(self, ai_settings, screen, stats):
+    def __init__(self, ai_settings, screen, sounds, stats):
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
+        self.sounds = sounds
         self.stats = stats
 
-        self.text_color = (30, 30, 30)
+        self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
         self.prep_score()
@@ -47,7 +49,7 @@ class Scoreboard():
     def prep_ships(self):
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.ai_settings, self.screen)
+            ship = Ship(self.ai_settings, self.screen, self.sounds)
             ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
