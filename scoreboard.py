7b2dd6e47d21c183835f6052dfccbd3616f2e975
moralesalex89 <moralesalex89@yahoo.com>
2019-10-07 15:31:03 -0700

    Revert "Initial Push to Github"
    This reverts commit 8ddbc167944868a391dfe2dc2ca8d914caba68d5.

--- scoreboard.py
+++ scoreboard.py
@@ -3,17 +3,15 @@ from pygame.sprite import Group
 
 from ship import Ship
 
-
 class Scoreboard():
 
-    def __init__(self, ai_settings, screen, sounds, stats):
+    def __init__(self, ai_settings, screen, stats):
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
-        self.sounds = sounds
         self.stats = stats
 
-        self.text_color = (255, 255, 255)
+        self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
         self.prep_score()
@@ -49,7 +47,7 @@ class Scoreboard():
     def prep_ships(self):
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.ai_settings, self.screen, self.sounds)
+            ship = Ship(self.ai_settings, self.screen)
             ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
